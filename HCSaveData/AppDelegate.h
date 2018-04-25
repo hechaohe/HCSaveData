ea3a640def88793b3a3c60fd1be67dd4e3b41a32
hechao <chao.he@tbook.io>
2018-04-25 15:30:22 +0800

    Initial Commit

---
+++ HCSaveData/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  HCSaveData
+//
+//  Created by hc on 2018/4/25.
+//  Copyright © 2018年 ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ HCSaveData/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HCSaveData
+//
+//  Created by hc on 2018/4/25.
+//  Copyright © 2018年 ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
